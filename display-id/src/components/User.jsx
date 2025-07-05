701f4bd8fe8f9c94cf763bf857485675e45eb9c4
anandavallis <sanandavalli7@gmail.com>
2025-07-05 20:00:26 +0530

    Create User.jsx

---
+++ display-id/src/components/User.jsx
@@ -0,0 +1,16 @@
+// src/components/User.jsx
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const User = () => {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <h2>User Page</h2>
+      <p>User ID: {id}</p>
+    </div>
+  );
+};
+
+export default User;
